99123c2aea0cad304eec658141bbecb8cff91d19
Cong Zhang <congzhangzh@gmail.com>
2020-08-04 21:50:57 +0800

    adjust to work like a real windows application
    1. no consoe window
    2. open a browser automate when blazor server started
    3. exit blazor server when user disconnected, like navigation to other place or close browser windows

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -12,10 +13,38 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components.Server.Circuits;
 using Microsoft.Extensions.FileProviders;
 
 namespace BlazorApp1
 {
+    public class TrackingCircuitHandler : CircuitHandler
+    {
+        private HashSet<Circuit> circuits = new HashSet<Circuit>();
+
+        public override Task OnConnectionUpAsync(Circuit circuit,
+            CancellationToken cancellationToken)
+        {
+            //circuits.Add(circuit);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task OnConnectionDownAsync(Circuit circuit,
+            CancellationToken cancellationToken)
+        {
+            //circuits.Remove(circuit);
+
+            AppLifetime?.StopApplication();
+            return Task.CompletedTask;
+        }
+
+        //public int ConnectedCircuits => circuits.Count;
+
+        public static TrackingCircuitHandler Instance { get; }=new TrackingCircuitHandler();
+        public Microsoft.Extensions.Hosting.IHostApplicationLifetime AppLifetime;
+    }
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -32,11 +61,13 @@ namespace BlazorApp1
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<CircuitHandler>(TrackingCircuitHandler.Instance);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Microsoft.Extensions.Hosting.IHostApplicationLifetime appLifetime)
         {
+            TrackingCircuitHandler.Instance.AppLifetime = appLifetime;
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,6 +87,7 @@ namespace BlazorApp1
             var resourceAssemblyName = ass.GetName().Name;
             app.UseStaticFiles(new StaticFileOptions
                 {
+                    //[Tips] resourceAssemblyName is something like project name?
                     FileProvider = new EmbeddedFileProvider(ass, $"{resourceAssemblyName}.wwwroot"),
                     RequestPath = "/embeded"
                 }
@@ -67,7 +99,9 @@ namespace BlazorApp1
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+
             });
+
             appLifetime.ApplicationStarted.Register(() => OpenBrowser(
                 app.ServerFeatures.Get<Microsoft.AspNetCore.Hosting.Server.Features.IServerAddressesFeature>().Addresses.First()));
 
@@ -76,11 +110,28 @@ namespace BlazorApp1
         //[TODO]
         private static void OpenBrowser(string url)
         {
+
+            //TODO what's the safe way to detect the browser is closed and close the main application manualy?
             Process.Start(
                 new ProcessStartInfo("cmd", $"/c start {url}")
                 {
                     CreateNoWindow = true
                 });
+
+
+            // var p = new Process{
+            //     StartInfo = new ProcessStartInfo("cmd", $"/c start {url}")
+            //     {
+            //         CreateNoWindow = true
+            //     }
+            // };
+            // p.Start();
+            //
+            // Task.Factory.StartNew(new Action(() =>
+            // {
+            //     p.WaitForExit();
+            //     System.Environment.Exit(0);
+            // }));
         }
     }
 }
